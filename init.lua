1abf43843eadb265fe7828b59067cbe5581e7b45
fengjicheng <fengjicheng@jd.com>
2020-04-08 16:01:35 +0800

    环境变量初始化脚本

---
+++ init.lua
@@ -0,0 +1,10 @@
+global_configs = {
+  ["divEnable"] = false,
+  ["newTrafficRate"] = 0,
+  ["whiteList"] = '',
+  ["redis"] = {
+        ap_host = '***.***.***',
+        ap_port = ***,
+        ap_key = '***' -- POPCARE-LUA jimdb
+  }
+}
